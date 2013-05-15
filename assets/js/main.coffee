b09c1d47252a9bfba5bb9458f529afae9f41aac4
Henry <HHSnopek@gmail.com>
2013-05-15 12:37:21 -0500

    Finished.

--- assets/js/main.coffee
+++ assets/js/main.coffee
@@ -1,6 +1,6 @@
 require.config paths:
-	moment: "/components/moment/moment"
 	jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min"
+	impress: "/components/impress.js/js/impress"
 
-define ["moment", "jquery"], (moment, $) ->
-	return
+define ["jquery", "impress"], ($) ->
+	impress().init()
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 (function() {
   require.config({
     paths: {
-      moment: "/components/moment/moment",
-      jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min"
+      jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+      impress: "/components/impress.js/js/impress"
     }
   });
 
-  define(["moment", "jquery"], function(moment, $) {});
+  define(["jquery", "impress"], function($) {
+    return impress().init();
+  });
 
 }).call(this);
